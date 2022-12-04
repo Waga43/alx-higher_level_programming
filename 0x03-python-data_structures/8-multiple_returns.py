b3f8cd0d0cf75a55a52318449d3b23fa6d2b2b02
waga43 <waga43tech@gmail,com>
2022-12-04 00:21:32 -0800

    Script edited

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     """Returns the length of a string and its first character."""
+
     my_tuple = ()
     if len(sentence) == 0:
         my_tuple = 0, "None"

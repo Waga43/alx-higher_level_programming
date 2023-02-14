0cbb0e2ec06a0d0c95c3c74156b3fbaf4636e0dc
waga43 <waga43tech@gmail,com>
2023-02-14 14:11:13 -0800

    Added the FULL clause to the query

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,5 @@
 -- MySQL script that prints the full description of the table first_table
--- from the database hbtn_0c_0 in your MySQL server.
+-- from the database hbtn_0c_0 in  MySQL server.
 -- The database name will be passed as an argument of the mysql command
 -- You are not allowed to use the DESCRIBE or EXPLAIN statements
-SHOW COLUMNS FROM hbtn_0c_0.first_table;
+SHOW FULL COLUMNS FROM hbtn_0c_0.first_table;

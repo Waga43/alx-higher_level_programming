0555ac8e80a37c9d0db6d82071fefb7173522ecf
waga43 <waga43tech@gmail,com>
2023-02-14 14:22:17 -0800

    Edited the 5-full_table_sql file

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -2,4 +2,4 @@
 -- from the database hbtn_0c_0 in  MySQL server.
 -- The database name will be passed as an argument of the mysql command
 -- You are not allowed to use the DESCRIBE or EXPLAIN statements
-SHOW CREATE TABLE FROM hbtn_0c_0.first_table;
+SHOW CREATE TABLE first_table;

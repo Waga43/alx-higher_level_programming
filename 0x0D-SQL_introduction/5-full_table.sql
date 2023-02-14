94b5d3cc5a7236753a9d4b1892ccae4e9aac6b65
waga43 <waga43tech@gmail,com>
2023-02-14 14:12:52 -0800

    Limited script to 2 lines

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,3 @@
 -- MySQL script that prints the full description of the table first_table
 -- from the database hbtn_0c_0 in  MySQL server.
--- The database name will be passed as an argument of the mysql command
--- You are not allowed to use the DESCRIBE or EXPLAIN statements
 SHOW FULL COLUMNS FROM hbtn_0c_0.first_table;

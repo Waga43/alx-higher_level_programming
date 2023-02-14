bf5e59f236e33c99bd391c822d9d31be9ad2a99b
waga43 <waga43tech@gmail,com>
2023-02-14 14:20:54 -0800

    Used the SHOW CREATE TABLE command

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,5 @@
 -- MySQL script that prints the full description of the table first_table
 -- from the database hbtn_0c_0 in  MySQL server.
-SHOW FULL COLUMNS FROM hbtn_0c_0.first_table;
+-- The database name will be passed as an argument of the mysql command
+-- You are not allowed to use the DESCRIBE or EXPLAIN statements
+SHOW CREATE TABLE FROM hbtn_0c_0.first_table;

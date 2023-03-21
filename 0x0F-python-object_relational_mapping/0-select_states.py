cc77ebfe9b3461ab597156e64d910643ec22eb2d
waga43 <waga43tech@gmail,com>
2023-03-21 14:24:05 -0700

    Sys.argv edited

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,10 +8,9 @@ import sys
     Command line arguments for MySQL username,
     MySQL password, and database name
 '''
-_args = sys.argv
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host = 'localhost', port = 3306, user = _argv[1], passwd = _argv[2], db = _argv[3])
+    conn = MySQLdb.connect(host = 'localhost', port = 3306, user = sys.argv[1], passwd = sys.argv[2], db = sys.argv[3])
     cur = conn.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     query_rows = cur.fetchall()

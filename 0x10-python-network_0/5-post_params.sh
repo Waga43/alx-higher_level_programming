8be63916377531f5767e5a59a4fafb591c5807b4
waga43 <waga43tech@gmail,com>
2023-03-30 14:40:23 -0700

    Edited script email variable

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Sends a POST request to a URL, and displays the body of the response
-curl "$1" -sX POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
+curl "$1" -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD"
 

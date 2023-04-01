6080e554dec550bfc48f02f92f2f0cd889c19c1e
waga43 <waga43tech@gmail,com>
2023-03-31 19:48:27 -0700

    8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Python script:
+- takes in a letter
+- sends POST request to http://0.0.0.0:5000/search_user
+- with the letter as a parameter.
+"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    if len(argv) == 1:
+        letter = ""
+    else:
+        letter = argv[[1]
+    payload = {"q": letter}
+
+    response = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+    try:
+       json_ response = response.json()
+        if json_response == {}:
+            print("No result")
+        else:
+            print(f"[{json_response.get('id')}] {json_response.get('name')}")
+    except ValueError:
+        print("Not a valid JSON")
